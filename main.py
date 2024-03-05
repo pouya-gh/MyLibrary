0789a0f656523fa0469830f32ebc20dd6d915d2a
Pouya Gharibpour <p.gharibpour@gmail.com>
2024-03-05 11:44:23 +0330

    add authentication requirement to create and update path operations (except User create) with some function readability improvement attemps

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from datetime import timedelta
 from sql import crud, models, schemas
 from sql.database import engine
 import dependencies
-from dependencies import get_db
+from dependencies import get_db, get_current_active_user
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -29,8 +29,10 @@ async def index():
 
 @app.post("/token")
 def login_for_access_token(
-    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
-    db: Annotated[Session, Depends(get_db)]):
+        form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+        db: Annotated[Session, Depends(get_db)]
+    ):
+    
     user = authenticate_user(db, form_data.username, form_data.password)
     if not user:
         raise HTTPException(
@@ -46,38 +48,63 @@ def login_for_access_token(
     return {"access_token": access_token, "token_type":"bearer"}
 
 @app.get("/users/me/", response_model=schemas.User)
-def get_current_logged_in_user(current_user: Annotated[schemas.User, Depends(dependencies.get_current_active_user)]):
+def get_current_logged_in_user(
+        current_user: Annotated[schemas.User, Depends(get_current_active_user)]
+    ):
+
     return current_user
 
 # users
 @app.post('/users/', response_model=schemas.User)
-def create_user(db: Annotated[Session, Depends(get_db)], user: schemas.UserCreate):
+def create_user(
+        db: Annotated[Session, Depends(get_db)], 
+        user: schemas.UserCreate
+    ):
+
     try:
         return crud.create_user(db, user)
     except IntegrityError:
         raise HTTPException(status_code=400, detail="User email/username already exists")
 
 @app.get("/users/", response_model=list[schemas.User])
-def get_users(db: Annotated[Session, Depends(get_db)], skip: int = 0, limit: int = 100):
+def get_users(
+        db: Annotated[Session, Depends(get_db)], 
+        skip: int = 0, limit: int = 100
+    ):
+
     users = crud.get_users(db, skip, limit)
     return users
 
 @app.get("/users/{user_id}", response_model=schemas.User)
-def get_user(db: Annotated[Session, Depends(get_db)], user_id: int):
+def get_user(
+        db: Annotated[Session, Depends(get_db)], 
+        user_id: int
+    ):
+
     db_user = crud.get_user(db, user_id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User does not exist")
     return db_user
 
 @app.post("/users/{user_id}", response_model=schemas.User)
-def update_user(db: Annotated[Session, Depends(get_db)], data: schemas.UserUpdate, user_id: int):
+def update_user(
+        db: Annotated[Session, Depends(get_db)],
+        current_user: Annotated[schemas.User, Depends(get_current_active_user)],
+        data: schemas.UserUpdate, user_id: int
+    ):
+
     db_user = crud.update_user(db, user_id, data)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User does not exist")
     return db_user
 
 @app.post("/users/{user_id}/delete", response_model=schemas.User)
-def delete_user(db: Annotated[Session, Depends(get_db)], user_id: int):
+def delete_user(
+        db: Annotated[Session, Depends(get_db)], 
+        current_user: Annotated[schemas.User, Depends(get_current_active_user)],
+        user_id: int
+    ):
+
     db_user = crud.delete_user(db, user_id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User does not exist")
@@ -85,32 +112,55 @@ def delete_user(db: Annotated[Session, Depends(get_db)], user_id: int):
 
 # authors
 @app.post("/authors/", response_model=schemas.Author)
-def create_author(db: Annotated[Session, Depends(get_db)], author: schemas.AuthorCreate):
+def create_author(
+        db: Annotated[Session, Depends(get_db)], 
+        current_user: Annotated[schemas.User, Depends(get_current_active_user)],
+        author: schemas.AuthorCreate
+    ):
+
     try:
         return crud.create_author(db, author)
     except IntegrityError:
         raise HTTPException(status_code=400, detail="Failed to create author")
     
 @app.get("/authors/", response_model=list[schemas.Author])
-def get_authors(db: Annotated[Session, Depends(get_db)], skip: int = 0, limit: int = 100):
+def get_authors(
+        db: Annotated[Session, Depends(get_db)], 
+        skip: int = 0, limit: int = 100
+    ):
+
     return crud.get_authors(db, skip, limit)
 
 @app.get("/authors/{author_id}", response_model=schemas.Author)
-def get_author(db: Annotated[Session, Depends(get_db)], author_id: int):
+def get_author(
+        db: Annotated[Session, Depends(get_db)], 
+        author_id: int
+    ):
+
     db_author = crud.get_author(db, author_id)
     if db_author is None:
         raise HTTPException(status_code=404, detail="Author deos not exist")
     return db_author
 
 @app.post("/authors/{author_id}", response_model=schemas.Author)
-def update_author(db: Annotated[Session, Depends(get_db)], author: schemas.AuthorUpdate, author_id: int):
+def update_author(
+        db: Annotated[Session, Depends(get_db)], 
+        current_user: Annotated[schemas.User, Depends(get_current_active_user)],
+        author: schemas.AuthorUpdate, author_id: int
+    ):
+    
     db_author = crud.update_author(db, author_id, author)
     if db_author is None:
         raise HTTPException(status_code=404, detail="Author does not exist")
     return db_author
 
 @app.post("/authors/{author_id}/delete", response_model=schemas.Author)
-def delete_author(db: Annotated[Session, Depends(get_db)], author_id: int):
+def delete_author(
+        db: Annotated[Session, Depends(get_db)], 
+        current_user: Annotated[schemas.User, Depends(get_current_active_user)],
+        author_id: int
+    ):
+
     db_author = crud.delete_author(db, author_id)
     if db_author is None:
         raise HTTPException(status_code=404, detail="Author does not exist")
@@ -118,25 +168,44 @@ def delete_author(db: Annotated[Session, Depends(get_db)], author_id: int):
 
 # genre
 @app.post("/genres/", response_model=schemas.Genre)
-def create_genre(db: Annotated[Session, Depends(get_db)], genre: schemas.GenreCreate):
+def create_genre(
+        db: Annotated[Session, Depends(get_db)], 
+        current_user: Annotated[schemas.User, Depends(get_current_active_user)],
+        genre: schemas.GenreCreate
+    ):
+
     try:
         return crud.create_genre(db, genre)
     except IntegrityError:
         raise HTTPException(status_code=400, detail="Failed to create genre")
     
 @app.get("/genres/", response_model=list[schemas.Genre])
-def get_genres(db: Annotated[Session, Depends(get_db)], skip: int = 0, limit: int = 100):
+def get_genres(
+        db: Annotated[Session, Depends(get_db)], 
+        skip: int = 0, limit: int = 100
+    ):
+
     return crud.get_genres(db, skip, limit)
 
 @app.post("/genres/{genre_id}", response_model=schemas.Genre)
-def update_genre(db: Annotated[Session, Depends(get_db)], genre: schemas.GenreUpdate, genre_id: int):
+def update_genre(
+        db: Annotated[Session, Depends(get_db)], 
+        current_user: Annotated[schemas.User, Depends(get_current_active_user)],
+        genre: schemas.GenreUpdate, genre_id: int
+    ):
+
     db_genre = crud.update_genre(db, genre_id, genre)
     if db_genre is None:
         raise HTTPException(status_code=404, detail="Genre does not exist")
     return db_genre
 
 @app.post("/genres/{genre_id}/delete", response_model=schemas.Genre)
-def delete_genre(db: Annotated[Session, Depends(get_db)], genre_id: int):
+def delete_genre(
+        db: Annotated[Session, Depends(get_db)], 
+        current_user: Annotated[schemas.User, Depends(get_current_active_user)],
+        genre_id: int
+    ):
+
     db_genre = crud.delete_genre(db, genre_id)
     if db_genre is None:
         raise HTTPException(status_code=404, detail="Genre does not exist")
@@ -144,25 +213,44 @@ def delete_genre(db: Annotated[Session, Depends(get_db)], genre_id: int):
 
 # language
 @app.post("/languages/", response_model=schemas.Language)
-def create_language(db: Annotated[Session, Depends(get_db)], language: schemas.LanguageCreate):
+def create_language(
+        db: Annotated[Session, Depends(get_db)], 
+        current_user: Annotated[schemas.User, Depends(get_current_active_user)],
+        language: schemas.LanguageCreate
+    ):
+
     try:
         return crud.create_language(db, language)
     except IntegrityError:
         raise HTTPException(status_code=400, detail="Failed to create language")
     
 @app.get("/languages/", response_model=list[schemas.Language])
-def get_languages(db: Annotated[Session, Depends(get_db)], skip: int = 0, limit: int = 100):
+def get_languages(
+        db: Annotated[Session, Depends(get_db)], 
+        skip: int = 0, limit: int = 100
+    ):
+
     return crud.get_languages(db, skip, limit)
 
 @app.post("/languages/{language_id}", response_model=schemas.Language)
-def update_language(db: Annotated[Session, Depends(get_db)], language: schemas.LanguageUpdate, language_id: int):
+def update_language(
+        db: Annotated[Session, Depends(get_db)], 
+        current_user: Annotated[schemas.User, Depends(get_current_active_user)],
+        language: schemas.LanguageUpdate, language_id: int
+    ):
+
     db_language = crud.update_language(db, language_id, language)
     if db_language is None:
         raise HTTPException(status_code=404, detail="Language does not exist")
     return db_language
 
 @app.post("/languages/{language_id}/delete", response_model=schemas.Language)
-def delete_language(db: Annotated[Session, Depends(get_db)], language_id: int):
+def delete_language(
+        db: Annotated[Session, Depends(get_db)], 
+        current_user: Annotated[schemas.User, Depends(get_current_active_user)],
+        language_id: int
+    ):
+
     db_language = crud.delete_language(db, language_id)
     if db_language is None:
         raise HTTPException(status_code=404, detail="Language does not exist")
@@ -170,32 +258,55 @@ def delete_language(db: Annotated[Session, Depends(get_db)], language_id: int):
 
 # book
 @app.post("/books/", response_model=schemas.Book)
-def create_book(db: Annotated[Session, Depends(get_db)], book: schemas.BookCreate):
+def create_book(
+        db: Annotated[Session, Depends(get_db)], 
+        current_user: Annotated[schemas.User, Depends(get_current_active_user)],
+        book: schemas.BookCreate
+    ):
+
     try:
         return crud.create_book(db, book)
     except IntegrityError:
         raise HTTPException(status_code=400, detail="Failed to create book")
     
 @app.get("/books/", response_model=list[schemas.Book])
-def get_books(db: Annotated[Session, Depends(get_db)], skip: int = 0, limit: int = 100):
+def get_books(
+        db: Annotated[Session, Depends(get_db)],
+        skip: int = 0, limit: int = 100
+    ):
+
     return crud.get_books(db, skip, limit)
 
 @app.get("/books/{book_id}", response_model=schemas.Book)
-def get_book(db: Annotated[Session, Depends(get_db)], book_id: int):
+def get_book(
+        db: Annotated[Session, Depends(get_db)], 
+        book_id: int
+    ):
+
     db_book = crud.get_book(db, book_id)
     if db_book is None:
         raise HTTPException(status_code=404, detail="Book deos not exist")
     return db_book
 
 @app.post("/books/{book_id}", response_model=schemas.Book)
-def update_book(db: Annotated[Session, Depends(get_db)], book: schemas.BookUpdate, book_id: int):
+def update_book(
+        db: Annotated[Session, Depends(get_db)], 
+        current_user: Annotated[schemas.User, Depends(get_current_active_user)],
+        book: schemas.BookUpdate, book_id: int
+    ):
+
     db_book = crud.update_book(db, book_id, book)
     if db_book is None:
         raise HTTPException(status_code=404, detail="Book does not exist")
     return db_book
 
 @app.post("/books/{book_id}/delete", response_model=schemas.Book)
-def delete_book(db: Annotated[Session, Depends(get_db)], book_id: int):
+def delete_book(
+        db: Annotated[Session, Depends(get_db)], 
+        current_user: Annotated[schemas.User, Depends(get_current_active_user)],
+        book_id: int
+    ):
+
     db_book = crud.delete_book(db, book_id)
     if db_book is None:
         raise HTTPException(status_code=404, detail="Book does not exist")
@@ -203,32 +314,55 @@ def delete_book(db: Annotated[Session, Depends(get_db)], book_id: int):
 
 # book instance
 @app.post("/bookinstances/", response_model=schemas.BookInstance)
-def create_bookinstance(db: Annotated[Session, Depends(get_db)], instance: schemas.BookInstanceCreate):
+def create_bookinstance(
+        db: Annotated[Session, Depends(get_db)], 
+        current_user: Annotated[schemas.User, Depends(get_current_active_user)],
+        instance: schemas.BookInstanceCreate
+    ):
+
     try:
         return crud.create_book_instance(db, instance)
     except IntegrityError:
         raise HTTPException(status_code=400, detail="Failed to create book instance")
     
 @app.get("/bookinstances/", response_model=list[schemas.BookInstance])
-def get_bookinstances(db: Annotated[Session, Depends(get_db)], skip: int = 0, limit: int = 100):
+def get_bookinstances(
+        db: Annotated[Session, Depends(get_db)], 
+        skip: int = 0, limit: int = 100
+    ):
+
     return crud.get_book_instances(db, skip, limit)
 
 @app.get("/bookinstances/{instance_id}", response_model=schemas.BookInstance)
-def get_bookinstance(db: Annotated[Session, Depends(get_db)], instance_id: str):
+def get_bookinstance(
+        db: Annotated[Session, Depends(get_db)], 
+        instance_id: str
+    ):
+
     db_bookinstance = crud.get_book_instance(db, instance_id)
     if db_bookinstance is None:
         raise HTTPException(status_code=404, detail="Book instance deos not exist")
     return db_bookinstance
 
 @app.post("/bookinstances/{instance_id}", response_model=schemas.BookInstance)
-def update_bookinstance(db: Annotated[Session, Depends(get_db)], instance: schemas.BookInstanceUpdate, instance_id: str):
+def update_bookinstance(
+        db: Annotated[Session, Depends(get_db)], 
+        current_user: Annotated[schemas.User, Depends(get_current_active_user)],
+        instance: schemas.BookInstanceUpdate, instance_id: str
+    ):
+    
     db_bookinstance = crud.update_book_instance(db, instance_id, crud)
     if db_bookinstance is None:
         raise HTTPException(status_code=404, detail="Book instance does not exist")
     return db_bookinstance
 
 @app.post("/bookinstances/{instance_id}/delete", response_model=schemas.BookInstance)
-def delete_bookinstance(db: Annotated[Session, Depends(get_db)], instance_id: str):
+def delete_bookinstance(
+        db: Annotated[Session, Depends(get_db)], 
+        current_user: Annotated[schemas.User, Depends(get_current_active_user)], 
+        instance_id: str
+    ):
+
     db_bookinstance = crud.delete_book_instance(db, instance_id)
     if db_bookinstance is None:
         raise HTTPException(status_code=404, detail="Book does not exist")
