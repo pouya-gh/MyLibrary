52cd7c0976b2db5ead25d840f638d81810b3fcf2
Pouya Gharibpour <p.gharibpour@gmail.com>
2024-03-06 10:01:58 +0330

    add book instance borrow and return functionality for current user

--- routers/book_instances.py
+++ routers/book_instances.py
@@ -8,6 +8,8 @@ from sql import schemas, crud
 
 from dependencies import get_db, get_current_active_user
 
+from datetime import timedelta, datetime
+
 router = APIRouter(prefix="/bookinstances")
 
 
@@ -65,3 +67,43 @@ def delete_bookinstance(
     if db_bookinstance is None:
         raise HTTPException(status_code=404, detail="Book does not exist")
     return db_bookinstance
+
+@router.post("/{instance_id}/borrow", response_model=schemas.BookInstance)
+def borrow_book(
+        db: Annotated[Session, Depends(get_db)],
+        current_user: Annotated[schemas.User, Depends(get_current_active_user)],
+        instance_id: str
+    ):
+
+    instance_db = crud.get_book_instance(db, instance_id)
+    if instance_db is None:
+        raise HTTPException(status_code=404, detail="Book instance not found")
+    
+    if instance_db.status == schemas.BookInstanceStatus.a:
+        update_data = schemas.BookInstanceUpdate(
+            status=schemas.BookInstanceStatus.o,
+            borrower_id=current_user.id,
+            due_back=datetime.today().date() + timedelta(days=14))
+        return crud.update_book_instance(db, instance_id, update_data)
+    else:
+        raise HTTPException(status_code=400, detail="Book instance is not available")
+
+@router.post("/{instance_id}/return", response_model=schemas.BookInstance) 
+def return_book(
+        db: Annotated[Session, Depends(get_db)],
+        current_user: Annotated[schemas.User, Depends(get_current_active_user)],
+        instance_id: str
+    ):
+
+    instance_db = crud.get_book_instance(db, instance_id)
+    if instance_db is None:
+        raise HTTPException(status_code=404, detail="Book instance not found")
+    
+    if instance_db.status == schemas.BookInstanceStatus.o and \
+        instance_db.borrower_id == current_user.id:
+        update_data = schemas.BookInstanceUpdate(
+            status=schemas.BookInstanceStatus.a,
+            borrower_id=0)
+        return crud.update_book_instance(db, instance_id, update_data)
+    else:
+        raise HTTPException(status_code=400, detail="This book instance is not borrowed to you")
